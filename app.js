9c1d39c1c557c98f71eee3b60429f9baa61968b4
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2023-10-02 18:33:53 -0400

    Part 1 and 2.

---
+++ app.js
@@ -0,0 +1,26 @@
+// Part One
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getGif();
+    // const searchTerm = document.getElementById('searchInput').value;
+    // // Get API key by clicking "Create an App" the GIPHY Developer Dashboard
+    // const apiKey = 'UiT3hkedZuiC4QTTlzcvRD4j4eDddYAu';
+
+    // axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`)
+    //     .then(response => console.log(response.data))
+    //     .catch(error => console.log(error))
+}
+);
+// Part Two
+async function getGif() {
+    const searchTerm = document.getElementById('searchInput').value;
+    // // Get API key by clicking "Create an App" the GIPHY Developer Dashboard
+    const apiKey = 'UiT3hkedZuiC4QTTlzcvRD4j4eDddYAu';
+
+    let res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`)
+    form.append(res.data);
+}
+
+// console.log("Let's get this party started!");
