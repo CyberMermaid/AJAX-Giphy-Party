ef797b619eb7e4cdcb2b8f7c8d5638b1ce95efc5
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2023-10-02 19:55:17 -0400

    Update Part 2

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ async function getGif() {
     // // Get API key by clicking "Create an App" the GIPHY Developer Dashboard
     const apiKey = 'UiT3hkedZuiC4QTTlzcvRD4j4eDddYAu';
 
-    let res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`)
+    let res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`);
     form.append(res.data);
+    // let img = document.createElement('img');
+    // img.src = res.data.images.original[url];
+    // form.append(img);
 }
 
 // console.log("Let's get this party started!");
